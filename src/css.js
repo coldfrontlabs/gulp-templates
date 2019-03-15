5426783195e1fa4674c1aa3eb9fda099c565e63b
Nick Milton <nickdjm@gmail.com>
2019-03-15 14:03:36 -0400

    feat(css): add lint function

--- src/css.js
+++ src/css.js
@@ -7,9 +7,30 @@ const autoprefixer = require('autoprefixer')
 const cssnano = require('cssnano')
 const comments = require('postcss-discard-comments')
 const rename = require('gulp-rename')
+const stylelint = require('gulp-stylelint')
 
 // Declare base functions.
 const css = {
+  /**
+   * Runs stylelint on a provided source.
+   *
+   * @param   {(String | String[])} source        - The source path(s).
+   * @param   {Boolean}             [fix = false] - Toggle the fix option for stylelint.
+   *
+   * @returns {Object} - Gulp stream.
+   */
+  lint: (source, fix = false) => {
+    const stream = src(source, { base: './' })
+      .pipe(stylelint({
+        reporters: [{ formatter: 'verbose', console: true }],
+        fix: fix
+      }))
+
+    // Only pipe out to destination if fix is enabled.
+    if (fix) stream.pipe(dest('.'))
+
+    return stream
+  },
   /**
    * Runs postcss/autoprefixer on a provided source and outputs the result.
    *
