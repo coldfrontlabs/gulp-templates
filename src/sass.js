02c0b0b049aa7595ccdee6bb6dff20a2f61bf141
Nick Milton <nickdjm@gmail.com>
2019-04-12 14:25:57 -0400

    feat(lint): split out fix into its own function

--- src/sass.js
+++ src/sass.js
@@ -10,35 +10,44 @@ const sass = {
   /**
    * Runs stylelint on a provided source.
    *
-   * @param   {(String | String[])} source        - The source path(s).
-   * @param   {Boolean}             [fix = false] - Toggle the fix option for stylelint.
+   * @param   {(String | String[])} source - The source path(s).
    *
    * @returns {Object} - Gulp stream.
    */
-  lint: (source, fix = false) => {
-    const stream = src(source, { base: './' })
+  lint: (source) => {
+    return src(source)
+      .pipe(stylelint({
+        reporters: [{ formatter: 'verbose', console: true }]
+      }))
+  },
+  /**
+   * Runs stylelint:fix on a provided source and outputs the result.
+   *
+   * @param   {(String | String[])} source               - The source path(s).
+   * @param   {String | Null}       [destination = null] - The destination path.
+   *
+   * @returns {Object} - Gulp stream.
+   */
+  fix: (source, destination = null) => {
+    return src(destination ? source : [source, { base: './' }])
       .pipe(stylelint({
         reporters: [{ formatter: 'verbose', console: true }],
-        fix: fix
+        fix: true
       }))
-
-    // Only pipe out to destination if fix is enabled.
-    if (fix) stream.pipe(dest('.'))
-
-    return stream
+      .pipe(dest(destination || '.'))
   },
   /**
    * Runs sass and on a provided source and outputs the result.
    *
-   * @param   {(String | String[])} source      - The source path(s).
-   * @param   {String}              destination - The destination path.
+   * @param   {(String | String[])} source               - The source path(s).
+   * @param   {String | Null}       [destination = null] - The destination path.
    *
    * @returns {Object} - Gulp stream.
    */
-  compile: (source, desination) => {
-    return src(source)
+  compile: (source, destination = null) => {
+    return src(destination ? source : [source, { base: './' }])
       .pipe(gulpSass().on('error', gulpSass.logError))
-      .pipe(dest(desination));
+      .pipe(dest(destination || '.'));
   }
 }
 
