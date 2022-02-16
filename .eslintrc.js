bcb673a61b1c0316f1e63bd2ddcd0802e6bc7078
Nick Milton <nmilton@coldfrontlabs.ca>
2022-02-16 12:22:15 -0500

    build: update lint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,13 @@
 module.exports = {
   env: {
     commonjs: true,
-    es6: true,
+    es2021: true,
     node: true,
   },
   extends: ["standard", "plugin:jsdoc/recommended", "prettier"],
   plugins: ["jsdoc"],
-  globals: {
-    Atomics: "readonly",
-    SharedArrayBuffer: "readonly",
-  },
   parserOptions: {
-    ecmaVersion: 2018,
+    ecmaVersion: "latest",
   },
   ignorePatterns: ["!.*.js"],
-  rules: {},
 };
