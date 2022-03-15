dfc682a495d5eff77650d2365ba6513fc3f25ff0
Nick Milton <nmilton@coldfrontlabs.ca>
2022-03-15 16:00:51 -0400

    feat(lib): add rename, default rename function, and rename customization to fetch
    BREAKING CHANGE: lib.fetch will run a default rename function to change all @ symbols to _ in the
    directory path

--- src/lib.js
+++ src/lib.js
@@ -1,14 +1,24 @@
 const { src, dest } = require("gulp");
 
+// Include gulp plugins.
+const rename = require("gulp-rename");
+
 const lib = {
   /**
    * Gathers source files and outputs them to a provided destination.
+   *
+   * **Note:** if neither renameOptions or renameFunction are changed from their
+   * default values a default function will run changing all "@" symbols in the
+   * directory path to "_"s. This is to resolve an issue with some server not
+   * being able to properly parse the encoded "@" symbol.
    *
    * @param   {object}          param0                           - The path options.
    * @param   {string|string[]} param0.source                    - The source path(s).
    * @param   {string}          param0.destination               - The destination path.
    * @param   {object}          [param0.sourceOptions = {}]      - Options for the source.
    * @param   {object}          [param0.destinationOptions = {}] - Options for the destination.
+   * @param   {object}          [param0.renameOptions = {}]      - Options for renaming.
+   * @param   {!Function}       [param0.renameFunction = null]   - A function to pass to ranme.
    * @returns {object} - Gulp stream.
    */
   fetch: ({
@@ -16,10 +26,28 @@ const lib = {
     destination,
     sourceOptions = {},
     destinationOptions = {},
+    renameOptions = {},
+    renameFunction = null,
   }) => {
-    return src(source, sourceOptions).pipe(
-      dest(destination, destinationOptions)
-    );
+    let stream = src(source, sourceOptions);
+
+    if (renameFunction) {
+      /* eslint-disable-next-line no-undef */
+      stream = stream.pipe(rename(path => renameFunction(path)));
+    } else if (Object.keys(renameOptions).length === 0) {
+      /* eslint-disable-next-line no-undef */
+      stream = stream.pipe(
+        rename(path => {
+          path.dirname = path.dirname.replace("@", "_");
+        })
+      );
+    } else {
+      stream = stream.pipe(rename(renameOptions));
+    }
+
+    stream.pipe(dest(destination, destinationOptions));
+
+    return stream;
   },
 };
 
